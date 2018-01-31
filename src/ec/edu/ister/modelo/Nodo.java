350518e3ce78d3a9372c33e6a687cdc21026530a
Cristian Calle <davidbsc14@gmail.com>
2018-01-30 21:23:45 -0500

    programa en java deber #5

---
+++ src/ec/edu/ister/modelo/Nodo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ec.edu.ister.modelo;
+
+/**
+ *
+ * Nombre: Ayo Esteban 
+ */
+public class Nodo {
+     int dato;
+    Nodo enlace;
+    public Nodo getEnlace;
+ public Nodo(int x){
+     dato = x;
+     enlace = null;
+}
+  public int getDato(){
+     return dato;
+}
+  public Nodo getEnlace()
+{
+    return enlace;
+}
+  public void setEnlace(Nodo enlace)
+{
+    this.enlace = enlace;
+  }
+
+}
